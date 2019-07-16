1a44d082187dff75c811c8499dc36595051d2d94
Ben Weintraub <bweintraub@achtung02.ccs.neu.edu>
2019-07-16 14:35:10 -0400

    fix filepaths

--- speedy-murmurs-simulator/attack_common.py
+++ speedy-murmurs-simulator/attack_common.py
@@ -12,50 +12,34 @@ def run_config(config_dict, output_dir, force=False):
 
     base = os.getcwd() + f'/{simulation_common.data_root}/' + simulation_common.get_output_base_path(config_dict)
 
-    # skip run if it has already happened
-    if not force:
-        tmp_path = None
-        if config_dict['simulation_type'] == "static":
-            tmp_path = simulation_common.get_static_data_path_config(config_dict)
-
-        elif config_dict['simulation_type'] == "dynamic":
-            tmp_path = simulation_common.get_dynamic_data_path_config(config_dict)
-
-        else:
-            # print(f"Invalid simulation type: {config_dict['simulation_type']}")
-            return f"Invalid simulation type: {config_dict['simulation_type']}"
-
-
-        if os.path.isfile(base + tmp_path[0] + tmp_path[1] + '/' + simulation_common.singles):
-            # print('Run exists. Skipping...')
-            return 'Run exists. Skipping...'
-
-    algo = config_dict['routing_algorithm']
-
-
     data_path = None
     if config_dict['simulation_type'] == "static":
         data_path = simulation_common.get_static_data_path_config(config_dict)
     elif config_dict['simulation_type'] == "dynamic":
         data_path = simulation_common.get_dynamic_data_path_config(config_dict)
     else:
-        # print(f"Invalid simulation type: {config_dict['simulation_type']}")
         return f"Invalid simulation type: {config_dict['simulation_type']}"
 
-    # if directory exists without file, then delete the directory
     path = base + data_path[0] + data_path[1]
+    # skip run if it has already happened
+    if not force and os.path.isfile(path + simulation_common.singles):
+        return 'Run exists. Skipping...'
 
-    if os.path.isdir(base + data_path[0]):
+    algo = config_dict['routing_algorithm']
+
+    # if directory exists without file, then delete the directory
+    if os.path.isdir(base + data_path[0]): ## TODO why isn't old path getting deleted?
         if not os.path.isfile(path + simulation_common.singles):
-            shutil.rmtree(path)
+            shutil.rmtree(base + data_path[0], ignore_errors=True)
 
     sim_type = config_dict['simulation_type']
     out = subprocess.run(['java', '-cp', f'{simulation_common.classpath}', f'{simulation_common.run_info[sim_type]["class"]}', f'{output_dir}'], capture_output=True)
-    return str(out) + str(config_dict)
-    # if it fails, delete dir and try one more time
+    # return str(out) + str(config_dict)
+
+    # if it fails, delete dir
     if out.returncode == 1:
-        shutil.rmtree(path)
-        out = "Rerun: " + str(subprocess.run(['java', '-cp', f'{simulation_common.classpath}', f'{simulation_common.run_info[sim_type]["class"]}', f'{output_dir}'], capture_output=True))
+        shutil.rmtree(base + data_path[0], ignore_errors=True)
+        # out = "Rerun: " + str(subprocess.run(['java', '-cp', f'{simulation_common.classpath}', f'{simulation_common.run_info[sim_type]["class"]}', f'{output_dir}'], capture_output=True))
 
     # this is what get's printed by ipyparallel
     return str(out) + str(config_dict)
