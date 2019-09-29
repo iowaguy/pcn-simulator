bcd0a9f9092ed2749cba6f94fd49385ac0edd1a6
Ben Weintraub <bweintraub@achtung02.ccs.neu.edu>
2019-09-29 17:36:11 -0400

    Fix wrong algo type in scripts

--- speedy-murmurs-simulator/ff-dyn-runs-variable-epoch.sh
+++ speedy-murmurs-simulator/ff-dyn-runs-variable-epoch.sh
@@ -3,7 +3,7 @@
 for epoch in 10 20 30 50 100; do
     for i in `seq 0 2`; do
 	printf "epoch: $epoch\nstep: $i\n"
-	java -cp ff-variable-epoch-jars/pcn-simulator-1.0-SNAPSHOT-jar-with-dependencies-${epoch}.jar treeembedding.tests.Dynamic 0 0 $i 
+	java -cp ff-variable-epoch-jars/pcn-simulator-1.0-SNAPSHOT-jar-with-dependencies-${epoch}.jar treeembedding.tests.Dynamic 0 10 $i 
     done
 done
 
