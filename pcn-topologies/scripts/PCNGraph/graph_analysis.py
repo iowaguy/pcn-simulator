0f2dbcc5a0ecd279758c6a4932c330de2d3675f0
Ben Weintraub <benweintraub34@gmail.com>
2020-10-03 11:48:45 -0400

    Add new scripts and refactor module

---
+++ pcn-topologies/scripts/PCNGraph/graph_analysis.py
@@ -0,0 +1,44 @@
+from typing import Dict, Tuple
+import networkx as nx
+import matplotlib.pyplot as plt
+
+first_data_line_in_topo = 7
+betweenness_centrality = "betweenness_centrality"
+
+selection_methods = {betweenness_centrality: nx.betweenness_centrality}
+
+def __parse_gtna_topology_line(line: str) -> Tuple:
+    node, rest = line.split(":")
+    connections = rest.strip().split(";")
+    return (node, connections)
+
+def show(G):
+    nx.draw(G, pos=nx.spring_layout(G))
+    plt.show()
+
+def parse_topology(filename):
+    with open(filename, 'r') as f:
+        i = 0
+        graph_dict = {}
+        for l in f.readlines():
+            # skip header
+            if i < first_data_line_in_topo:
+                i+=1
+                continue
+            node, connections = __parse_gtna_topology_line(l)
+            if node not in graph_dict:
+                graph_dict[node] = {}
+
+            for con in connections:
+                graph_dict[node][con] = {"weight": 1}
+
+        return nx.Graph(graph_dict)
+
+def __sort_dict_by_keys(d):
+    return [(k, v) for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)]
+
+def select_n_by_method(basepath, method=betweenness_centrality, top_n=1):
+    filename = "/topology.graph"
+    G = parse_topology(basepath + filename)
+    top_n_entries = __sort_dict_by_keys(selection_methods[method](G))[0:top_n]
+    return ([int(k) for k, _ in top_n_entries], [v for _, v in top_n_entries])
