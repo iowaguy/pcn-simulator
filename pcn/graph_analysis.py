3ab1fde2a49001a234c1031677665af5b06cd431
Ben Weintraub <benweintraub34@gmail.com>
2020-11-28 17:14:39 -0500

    Turn var into string so I don't have to import it

--- pcn/graph_analysis.py
+++ pcn/graph_analysis.py
@@ -5,9 +5,9 @@ import networkx as nx
 from typing import Tuple
 
 first_data_line_in_topo = 7
-betweenness_centrality = "betweenness_centrality"
+betweenness_centrality = 'betweenness_centrality'
 
-selection_methods = {betweenness_centrality: nx.betweenness_centrality}
+selection_methods = {'betweenness_centrality': nx.betweenness_centrality}
 
 def __parse_gtna_topology_line(line: str) -> Tuple:
     node, rest = line.split(":")
@@ -39,7 +39,7 @@ def parse_topology(filename):
 def __sort_dict_by_keys(d):
     return [(k, v) for k, v in sorted(d.items(), key=lambda item: item[1], reverse=True)]
 
-def select_n_by_method(basepath, method=betweenness_centrality, top_n=1):
+def select_n_by_method(basepath, method='betweenness_centrality', top_n=1):
     filename = "/topology.graph"
     G = parse_topology(basepath + filename)
     top_n_entries = __sort_dict_by_keys(selection_methods[method](G))[0:top_n]
