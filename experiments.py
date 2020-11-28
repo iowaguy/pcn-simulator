52d9e3658dfaf17877615f8b68a31149b98dcfed
Ben Weintraub <weintraub.b@husky.neu.edu>
2020-11-28 18:00:36 -0500

    Add exp 53

--- experiments.py
+++ experiments.py
@@ -1508,6 +1508,27 @@ def get_experiments():
             "network_latency_ms":1,
             "force_overwrite": True            
         },
+        "53" : {
+            "notes" : "Select attackers by tree depth",
+            "num_steps":1,
+            "data_set_list":["id69-synthetic-poisson-nodes-10000-txs-pareto-100000-scalefree2-mult-0.75-prob-0.25"],
+            "concurrent_transactions_count":[10000],
+            "routing_algorithms":[common.speedymurmurs],
+            "epoch_lengths_list":[1],
+            "network_latency_ms":1,
+            "attack_type":["griefing_success"],
+            "receiver_delay_variability": 0,
+            "receiver_delay_ms":[10000],
+            "attacker_selection":"selected",
+            "selected_byzantine_nodes":[("baseline", 0),
+                                        ("by_tree_depth", 10),
+                                        ("by_tree_depth", 50),
+                                        ("by_tree_depth", 100),
+                                        ("by_tree_depth", 300),
+                                        ("by_tree_depth", 500)],
+            "exp_path":"data/dynamic-id69-53-prep/dynamic-id69-synthetic-poisson-nodes-10000-txs-pareto-100000-scalefree2-mult-0.75-prob-0.25-speedymurmurs-3-1-1-lat1ms-concurrent-10000-arrivalDelay0ms/READABLE_FILE_SM-P0-10000/",
+            "force_overwrite": True
+        },
         "test" : {
             "notes" : "Try out new topo that has random participant distro",
             "num_steps":1,
