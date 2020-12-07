4dcecbaba755e92122a070dad922191d4f07a478
Ben Weintraub <benweintraub34@gmail.com>
2020-12-07 15:42:20 -0500

    Select correct number of attackers

--- pcn/exp_analysis.py
+++ pcn/exp_analysis.py
@@ -27,24 +27,38 @@ def get_top_n_nodes_by_transaction_count(n, exp_path=None, tx_counts=[], roots=3
 
 def get_top_n_nodes_by_tree_depth(n, exp_path, roots=3):
     selected_nodes = pd.DataFrame()
+    total_to_select = n
     for i in range(roots):
+        
+        if total_to_select == 0:
+            break
+        elif total_to_select < 0:
+            raise Error("We should never select more nodes than intended.")
+
         df = pd.read_csv(exp_path + f"/INITIAL_graph.txt_SPANNINGTREE_{i}",
                          header=None, skiprows=6, sep=';',
                          names=['parent', 'index', 'depth'])
 
-        nodes_to_select_per_tree = int(math.ceil(float(n)/float(roots))) + 1
-        # want to select the same number of roots per tree
+        nodes_to_select_per_tree = int(math.ceil(float(n)/float(roots)))
+        # want to select the same number of nodes per tree
         for depth in range(1, df.max()['depth'] + 1):
             nodes_at_current_depth = df[(df.depth == depth)]
-            if len(nodes_at_current_depth) >= nodes_to_select_per_tree:
+            if total_to_select < nodes_to_select_per_tree:
+                if len(nodes_at_current_depth) >= total_to_select:
+                    select_these = nodes_at_current_depth.sample(total_to_select)
+                else:
+                    select_these = nodes_at_current_depth
+            elif len(nodes_at_current_depth) >= nodes_to_select_per_tree:
                 select_these = nodes_at_current_depth.sample(nodes_to_select_per_tree)
-                selected_nodes = pd.concat([selected_nodes, select_these])
-                nodes_to_select_per_tree -= len(select_these)
             else:
-                select_these = nodes_at_current_depth.sample(len(nodes_at_current_depth))
-                selected_nodes = pd.concat([selected_nodes, select_these]) 
-                nodes_to_select_per_tree -= len(select_these)
-            
+                select_these = nodes_at_current_depth
+
+            selected_nodes = pd.concat([selected_nodes, select_these]) 
+            nodes_to_select_per_tree -= len(select_these)
+            total_to_select -= len(select_these)
+            if total_to_select == 0:
+                break
+
     return selected_nodes.index.values.tolist()
 
 def get_transactions_per_node(exp_path):
